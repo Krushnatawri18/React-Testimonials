d89b9afd1e75187f95cf1f2116ee9913de762bc6
Krushna Tawri <krishtawri123@gmail.com>
2024-04-05 08:53:52 +0530

    Initialize project using Create React App

---
+++ data.js
@@ -0,0 +1,38 @@
+export const reviews = [
+    {
+        id:1,
+        name: 'Pranay Shetty',   
+        job: 'Software Engineer',
+        image: 'https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg',
+        text: 'I have working with this team on several projects, and I am consistently working hard so that I can work comfortably with the team and reach the goal given by the company.' 
+    },
+    {
+        id:2,
+        name: 'Pranay Gupta',    
+        job: 'Software Engineer',
+        image: 'https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg',
+        text: 'I have working with this team on several projects, and I am consistently working hard so that I can work comfortably with the team and reach the goal given by the company.' 
+        
+    },
+    {
+        id:3,
+        name: 'Pranay Shetty',   
+        job: 'Software Engineer',
+        image: 'https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg',
+        text: 'I have working with this team on several projects, and I am consistently working hard so that I can work comfortably with the team and reach the goal given by the company.'  
+    },
+    {
+        id:4,
+        name: 'Pranay Shetty',    
+        job: 'Software Engineer',
+        image: 'https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg',
+        text: 'I have working with this team on several projects, and I am consistently working hard so that I can work comfortably with the team and reach the goal given by the company.' 
+    },
+    {
+        id:5,
+        name: 'Pranay Shetty',    
+        job: 'Software Engineer',
+        image: 'https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg',
+        text: 'I have working with this team on several projects, and I am consistently working hard so that I can work comfortably with the team and reach the goal given by the company.' 
+    },
+]
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <h1>Our Testimonials</h1>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
